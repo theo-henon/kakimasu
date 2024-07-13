73f5421acbd8509937db5d87f5c840f1be15929f
LeLapinouAlpha <t-henon@hotmail.fr>
2024-07-13 23:46:16 +0200

    fix(Grayscale8Image): implement constructor

--- src/image/grayscale8_image.cc
+++ src/image/grayscale8_image.cc
@@ -2,6 +2,10 @@
 
 namespace kakimasu::image
 {
+    Grayscale8Image::Grayscale8Image(uint32_t width, uint32_t height)
+        : PixmapImage<1, BitDepth::BITS_8>(width, height)
+    {}
+
     uint8_t Grayscale8Image::get_scale(uint32_t x, uint32_t y) const
     {
         return bytes_[get_width() * y + x];
