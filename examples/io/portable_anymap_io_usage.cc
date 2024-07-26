9487ee7c2ac000356197c91cd309f5c9ef73434b
LeLapinouAlpha <t-henon@hotmail.fr>
2024-07-26 13:24:10 +0200

    docs(PortableAnymapWriter): write examples usage for PortableAnymapReader/Writer

--- examples/io/portable_anymap_io_usage.cc
+++ examples/io/portable_anymap_io_usage.cc
@@ -25,16 +25,54 @@ int main()
     img.set_pixel(1, 1, { 255, 255, 255 });
 
     // Write image in output streams
-    stdout_writer.write(img);
-    file_writer.write(img);
-
-    // Close file
+    stdout_writer.write(&img);
+    file_writer.write(&img);
     file.close();
 
-    // Create a reader for previously written image
+    // Create a reader for previously written image and write it to stdout
     std::ifstream previous_file("wikipedia.pnm");
     kio::PortableAnymapReader reader(previous_file);
     auto img_read = reader.read();
     std::cout << "\nImage read:\n";
-    stdout_writer.write(*dynamic_cast<kimg::Rgb24Image*>(img_read.get()));
+    stdout_writer.write(dynamic_cast<kimg::Rgb24Image*>(img_read.get()));
+
+    // Create a little grayscale image
+    kimg::Grayscale8Image gimg(2, 2);
+    gimg.set_pixel(0, 0, kimg::Grayscale8Color(0));
+    gimg.set_pixel(1, 0, kimg::Grayscale8Color(64));
+    gimg.set_pixel(0, 1, kimg::Grayscale8Color(128));
+    gimg.set_pixel(1, 1, kimg::Grayscale8Color(255));
+
+    // Create a writer for grayscale format to a file named "gray.pnm"
+    std::ofstream gofile("gray.pnm");
+    kio::PortableAnymapWriter gfile_writer(gofile);
+    gfile_writer.write(&gimg);
+    gofile.close();
+
+    // Create a reader for the previously written image
+    std::ifstream gifile("gray.pnm");
+    kio::PortableAnymapReader greader(gifile);
+    auto gimg_read = greader.read();
+    std::cout << "\nImage read:\n";
+    stdout_writer.write(dynamic_cast<kimg::Grayscale8Image*>(gimg_read.get()));
+
+    // Create a little bitmap image
+    kimg::BitmapImage bimg(2, 2);
+    bimg.set_pixel(0, 0, true);
+    bimg.set_pixel(1, 0, false);
+    bimg.set_pixel(0, 1, false);
+    bimg.set_pixel(1, 1, true);
+
+    // Create a writer for bitmap format to a file named "bitmap.pnm"
+    std::ofstream bofile("bitmap.pnm");
+    kio::PortableAnymapWriter bwriter(bofile);
+    bwriter.write(dynamic_cast<kimg::BitmapImage*>(&bimg));
+    bofile.close();
+
+    // Create a reader for the previously written image
+    std::ifstream bifile("bitmap.pnm");
+    kio::PortableAnymapReader breader(bifile);
+    auto bimg_read = breader.read();
+    std::cout << "\nImage read:\n";
+    stdout_writer.write(dynamic_cast<kimg::BitmapImage*>(bimg_read.get()));
 }
